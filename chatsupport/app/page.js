c2104d71c7380b3dbe20c89c5a23550aed4b21c2
JeremiahD9 <jeremiahdawson556@gmail.com>
2024-08-08 16:45:33 -0500

    Created the layout of the page

--- chatsupport/app/page.js
+++ chatsupport/app/page.js
@@ -1,5 +1,9 @@
 "use client"
-import { Box, Stack, TextField, Button} from "@mui/material";
+import * as React from 'react';
+import { Box, Stack, TextField, Button, AppBar, Toolbar, Typography, Container} from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 import {useState} from "react"
 
 export default function Home() {
@@ -8,6 +12,16 @@ export default function Home() {
     {role: "assistant", content: "Hi! Im your life assistant. How can I help you"},
   
   ])
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff8c7',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    margin: 5,
+    height: "250px",
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
   
   const sendMessage = async () => {
     setMessage('')
@@ -42,25 +56,67 @@ export default function Home() {
   const[message, setMessage] = useState("")
 
   return (
-    <Box width="100vw" height="100vh" display="flex" flexDirection={"column"} justifyContent={"center"} alignItems="center">
-      <Stack direction="column" width="500px" height="700px" border="1px solid black" p={2} spacing={3}>
-        <Stack direction="column" spacing={2} flexGrow={1} overflow={"auto"} maxHeight={"100%"}>
-          {
-            messages.map((message, index) => (
-              <Box key={index} display="flex" justifyContent={message.role === "assistant" ? "flex-start" : "flex-end"}>
-                <Box bgcolor={message.role === "assistant" ? "primary.main" : "secondary.main"} color="white" borderRadius={16} p={3}>
-                  {message.content}
+    <Box width="100vw" height="100vh" display="flex" flexDirection={"column"} overflow= "hidden">
+      <AppBar position="static" top sx={{marginBottom: 1}} >
+        <Toolbar>
+          <Typography variant="h4">Holiday Helper</Typography>
+        </Toolbar>
+      </AppBar>
+      <Stack direction="row" border="1.5px solid black">
+        <Stack direction="column" width="500px" height="650px" border="1.5px solid black" p={2} spacing={3} sx={{marginLeft: 1, borderRadius: 3}}>
+          <Stack direction="column" spacing={2} flexGrow={1} overflow={"auto"} maxHeight={"100%"}>
+            {
+              messages.map((message, index) => (
+                <Box key={index} display="flex" justifyContent={message.role === "assistant" ? "flex-start" : "flex-end"}>
+                  <Box bgcolor={message.role === "assistant" ? "primary.main" : "secondary.main"} color="white" borderRadius={16} p={3}>
+                    {message.content}
+                  </Box>
                 </Box>
-              </Box>
-            ))
-          }
+              ))
+            }
+          </Stack>
+          <Stack direction="row" spacing={2}>
+            <TextField label="Message" fullWidth value={message} onChange={(e) => {setMessage(e.target.value)}} />
+            <Button variant="contained" onClick={sendMessage}>
+              Send
+            </Button>
+          </Stack>
         </Stack>
-        <Stack direction="row" spacing={2}>
-          <TextField label="Message" fullWidth value={message} onChange={(e) => {setMessage(e.target.value)}} />
-          <Button variant="contained" onClick={sendMessage}>
-            Send
-          </Button>
-        </Stack>
+        <Container sx={{ border: "1.5px solid red", display: "flex", flexDirection: "column"}}>
+          <Box sx={{ border: "1.5px solid blue", height: "75px", marginTop: 2, width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+            Budget
+          </Box>
+          <Box border="1.5px solid green" width="100%" height= "100%" display="flex" flexDirection="row">
+            <Box border="1.5px solid purple" width="70%" height="100%">
+              <Grid container spacing={2} rowSpacing={5}>
+                <Grid xs={4}>
+                  <Item>Location</Item>
+                </Grid>
+                <Grid xs={4}>
+                  <Item>Hotel</Item>
+                </Grid>
+                <Grid xs={4}>
+                  <Item>Duration</Item>
+                </Grid>
+
+                <Grid xs={4}>
+                  <Item>Activies</Item>
+                </Grid>
+                <Grid xs={4}>
+                  <Item>Food</Item>
+                </Grid>
+                <Grid xs={4}>
+                  <Item>Travel</Item>
+                </Grid>
+              </Grid>
+            </Box>
+            <Box border="1.5px solid orange" width="30%" display="flex" flexDirection="column" justifyContent="center">
+              <Item style={{height: "100%"}}>
+                Extra Funds
+              </Item>
+            </Box>
+          </Box>
+        </Container>
       </Stack>
     </Box>
   );
